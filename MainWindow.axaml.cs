2c4f246bf133ad3b2006c88f0929bf805c11c2a9
Dan <nguyenngoctruongdan@gmail.com>
2025-10-16 07:40:48 +0700

    first commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace client_server_message;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
